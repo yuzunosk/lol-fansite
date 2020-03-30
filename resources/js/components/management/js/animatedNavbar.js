b400746f6f78dcb2b69e820ca22660096ad4f763
yuzunosk55 <yuzuha3ds@gmail.com>
2020-03-30 22:38:47 +0900

    animation追加

--- resources/js/components/management/js/animatedNavbar.js
+++ resources/js/components/management/js/animatedNavbar.js
@@ -1,89 +1,120 @@
+// alert('読み込めました');
+let foo = document.querySelector("#move");
+let news = document.querySelector("#lottie-news");
+let card = document.querySelector("#lottie-card");
 
-    // alert('読み込めました');
-    let foo = document.querySelector('#move');
-    let news = document.querySelector('#lottie-news');
-    
-    console.log({ foo });
-    console.log({ news });
+console.log({ foo });
+console.log({ news });
+console.log({ card });
 
-    // function doneAnimation(foo) {
-    // console.log('doneAnimationを開始');
+// function doneAnimation(foo) {
+// console.log('doneAnimationを開始');
 
 // -------------------------
 // lottie
 // -------------------------
 
-    lottie.loadAnimation({
-            container: document.getElementById('lottie'),
-            renderer: "svg",
-            loop: false,
-            autoplay: false,
-            path: "logo/menuicon2.json",
-            rendererSettings: {
-                className: 'anmIcon'
-            }
-        });
-        
-        foo.addEventListener('mouseenter', function() {
-            //マウスオーバーしたとき
-            console.log('mouseenterを発火しました');
+lottie.loadAnimation({
+    container: document.getElementById("lottie"),
+    renderer: "svg",
+    loop: false,
+    autoplay: false,
+    path: "logo/menuicon2.json",
+    rendererSettings: {
+        className: "anmIcon"
+    }
+});
 
-        lottie.setDirection(1); //通常再生に設定
-        lottie.setSpeed(2);
-        lottie.play(); //再生する
-    });
+foo.addEventListener("mouseenter", function() {
+    //マウスオーバーしたとき
+    console.log("mouseenterを発火しました");
 
-    foo.addEventListener("mouseleave", e => {
-        //マウスが離れたとき
-        console.log('mouseleaveを発火しました');
-        lottie.setDirection(-1); //逆再生に設定
-        lottie.setSpeed(2);
-        lottie.play(); //再生する
-    });
-// -------------------------    
-    // logo
+    lottie.setDirection(1); //通常再生に設定
+    lottie.setSpeed(2);
+    lottie.play(); //再生する
+});
+
+foo.addEventListener("mouseleave", e => {
+    //マウスが離れたとき
+    console.log("mouseleaveを発火しました");
+    lottie.setDirection(-1); //逆再生に設定
+    lottie.setSpeed(2);
+    lottie.play(); //再生する
+});
+// -------------------------
+// logo
 // -------------------------
 
-    // lottie.loadAnimation({
-    //     container: document.getElementById('lottie-logo'),
-    //     renderer: "svg",
-    //     loop: true,
-    //     autoplay: true,
-    //     path: "/resources/js/components/management/logo/yuzunosk.logo.json",
-    //     rendererSettings: {
-    //         className: 'anmLogo'
-    //     }
-    // });
+lottie.loadAnimation({
+    container: document.getElementById("lottie-logo"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "/resources/js/components/management/logo/yuzunosk.logo.json",
+    rendererSettings: {
+        className: "anmLogo"
+    }
+});
 // -------------------------
 // news
 // -------------------------
-//     lottie.loadAnimation({
-//         container: document.getElementById('lottie-news'),
-//         renderer: "svg",
-//         loop: false,
-//         autoplay: false,
-//         path: "/resources/js/components/management/logo/NEWS-icon-2.json",
-//         rendererSettings: {
-//             className: 'anmNews'
-//         }
-//     });
-//     // }
-    
-//     news.addEventListener('mouseenter', function() {
-//         //マウスオーバーしたとき
-//         console.log('mouseenterを発火しました2');
+lottie.loadAnimation({
+    container: document.getElementById("lottie-news"),
+    renderer: "svg",
+    loop: false,
+    autoplay: false,
+    path: "/resources/js/components/management/logo/NEWS-icon-2.json",
+    rendererSettings: {
+        className: "anmNews"
+    }
+});
 
-//     lottie.setDirection(1); //通常再生に設定
-//     lottie.setSpeed(2);
-//     lottie.play(); //再生する
-// });
+news.addEventListener("mouseenter", function() {
+    //マウスオーバーしたとき
+    console.log("mouseenterを発火しました2");
 
-// news.addEventListener("mouseleave", e => {
-//     //マウスが離れたとき
-//     console.log('mouseleaveを発火しました2');
-//     lottie.setDirection(-1); //逆再生に設定
-//     lottie.setSpeed(2);
-//     lottie.play(); //再生する
-// });
+    lottie.setDirection(1); //通常再生に設定
+    lottie.setSpeed(2);
+    lottie.play(); //再生する
+});
 
-// console.log('最後まで読み込みました');
+news.addEventListener("mouseleave", e => {
+    //マウスが離れたとき
+    console.log("mouseleaveを発火しました2");
+    lottie.setDirection(-1); //逆再生に設定
+    lottie.setSpeed(2);
+    lottie.play(); //再生する
+});
+
+// -------------------------
+// news
+// -------------------------
+lottie.loadAnimation({
+    container: document.getElementById("lottie-card"),
+    renderer: "svg",
+    loop: false,
+    autoplay: false,
+    path: "/resources/js/components/management/logo/card-mv.json",
+    rendererSettings: {
+        className: "anmNews"
+    }
+});
+
+card.addEventListener("mouseenter", function() {
+    //マウスオーバーしたとき
+    console.log("mouseenterを発火しました2");
+
+    lottie.setDirection(1); //通常再生に設定
+    lottie.setSpeed(2);
+    lottie.play(); //再生する
+});
+
+card.addEventListener("mouseleave", e => {
+    //マウスが離れたとき
+    console.log("mouseleaveを発火しました2");
+    lottie.setDirection(-1); //逆再生に設定
+    lottie.setSpeed(2);
+    lottie.play(); //再生する
+});
+
+console.log("最後まで読み込みました");
