51af3bb295c18f434c0407612867565dac318971
yuzunosk55 <yuzuha3ds@gmail.com>
2020-04-05 13:54:31 +0900

    mypage indexを修正

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,30 +13,31 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    // /**
-    //  * Create a new controller instance.
-    //  *
-    //  * @return void
-    //  */
-    // public function __construct()
-    // {
-    //     $this->middleware('auth');
-    // }
+  // /**
+  //  * Create a new controller instance.
+  //  *
+  //  * @return void
+  //  */
+  public function __construct()
+  {
+    $this->middleware('auth');
+  }
 
-    // /**
-    //  * Show the application dashboard.
-    //  *
-    //  * @return \Illuminate\Contracts\Support\Renderable
-    //  */
+  // /**
+  //  * Show the application dashboard.
+  //  *
+  //  * @return \Illuminate\Contracts\Support\Renderable
+  //  */
 
-     public function showHome() {
-        $chanpions = Chanpion::all();
-        $skills    = Skill::with('chanpion')->get();
-        // Log::info('ホームへのスキルデータ取得：'.$skills);
-        $tagBoxs      = TagBox::with('chanpion')->get();
-        $tags         = Tag::all();
-        return view('home', compact(['chanpions','skills','tagBoxs','tags']));
+  public function showHome()
+  {
+    $chanpions = Chanpion::all();
+    $skills    = Skill::with('chanpion')->get();
+    // Log::info('ホームへのスキルデータ取得：'.$skills);
+    $tagBoxs      = TagBox::with('chanpion')->get();
+    $tags         = Tag::all();
+    return view('home', compact(['chanpions', 'skills', 'tagBoxs', 'tags']));
 
-      //   return view('home', compact('chanpions'));
-     }
+    //   return view('home', compact('chanpions'));
+  }
 }
